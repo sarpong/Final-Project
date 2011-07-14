b70713d4b2621cf215ff6c555d2a1f9db5b6e67a
sarpong <kaysarP@yahoo.com>
2011-07-14 08:43:00 +0000

    views

--- mypark/park/views.py
+++ mypark/park/views.py
@@ -32,10 +32,9 @@ def loc_list(request):
 def purchase_spots(request, loc_id):
 	loc = Location.objects.get(pk=loc_id)
 	purchases = Purchase.objects.filter(location=loc)
-<<<<<<< HEAD
+
 	if request.method == 'POST':
 		form = PurchaseForm(request.POST)
-=======
 	nowTime = time.time()
 	no_available = loc.no_available
 	for purchase in purchases:
@@ -54,7 +53,7 @@ def purchase_spots(request, loc_id):
 
 	if request.method == 'POST': 
 		form = PurchaseForm(request.POST,instance=purchase)
->>>>>>> 84a323388e3e3327c01ca71d867514b539de48e8
+
 		if form.is_valid():
 			form.save()
 			return HttpResponseRedirect('/park/confirmation/'+str(purchase.id))
